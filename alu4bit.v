57c2cdc0286a0263f97d2e055647a8bc440f0bbc
Anmol Gautam <anmol.gautam@bison.howard.edu>
2019-02-11 05:41:33 +0000

    alu4bit.v: edited the ALU1 bit modules for syntactic accuracy and added always module for if condition

--- alu4bit.v
+++ alu4bit.v
@@ -9,16 +9,17 @@ module ALU4Bit(a, b, cin, less, op, result, cout, set, zero, g, p);
     supply0 gnd_wire;    
 
     // use ALI1Bit module for this addition to get value of result
-    ALU1Bit alu_0(a[0], b[0], cin, less, op, result[0],,, zero, g0, p0);
-    ALU1Bit alu_1(a[1], b[1], c1, gnd_wire, op, result[1],,, zero, g1, p1);
-    ALU1Bit alu_2(a[2], b[2], c2, gnd_wire, op, result[2],,, zero, g2, p2);
-    ALU1Bit alu_3(a[3], b[3], c3, gnd_wire, op, result[3],,mst_sig_bit, zero, g3, p3);
+    ALU1Bit alu_0(a[0], b[0], cin, less, op, result[0],,,, g0, p0);
+    ALU1Bit alu_1(a[1], b[1], c1, gnd_wire, op, result[1],,,, g1, p1);
+    ALU1Bit alu_2(a[2], b[2], c2, gnd_wire, op, result[2],,,, g2, p2);
+    ALU1Bit alu_3(a[3], b[3], c3, gnd_wire, op, result[3],,mst_sig_bit,, g3, p3);
 
     // call CLA module here. supply the inputs and get the outputs
     CLA cla_0(g0, p0, g1, p1, g2, p2, g3, p3, cin, c0, c1, c2, c3, c4, g, p);
 
     // set zero to 1 if the result is 0000 but set it to 0 if the result is not 0000
-    always @*
+    always @(result)
+    begin
         if (result[3:0] == 4'b0000) begin
             zero = 1;
         end else begin
